8c7000a95e9c85bfdd7d2e5e3053b299f6ea3174
DGKN <sakardogukan@gmail.com>
2023-12-15 23:03:59 +0300

    index.js file updated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 "use strict"
+
 /* -------------------- INDEX.JS ------------------ */
 //! Required Moduls:
 const express = require('express')
@@ -7,20 +8,61 @@ const app = express()
 require('dotenv').config()
 const PORT = process.env?.PORT || 8000
 const HOST = process.env?.HOST || '127.0.0.1'
-// console.log(process.env.PORT)
-// console.log(process.env.HOST)
+console.log(process.env.PORT)
+console.log(process.env.HOST)
 
 //! async-errors to errorHandler.js:
 require('express-async-errors')
-/* ------------------------------------------------ */
-
-
-
-
-
-
-
-
 
 /* ------------------------------------------------ */
-app.listen(PORT, () => console.log(`Running: http://${HOST}:${PORT}`))
+//! Configuration:
+
+//? Cors Config:
+var cors = require('cors')
+app.use(cors())
+
+//? DB Connection:
+const { dbConnection } = require('./src/config/dbConnection')
+dbConnection()
+
+/* ------------------------------------------------ */
+//! Middlewares:
+
+//? Accept JSON:
+app.use(express.json())
+
+
+
+//? Run Logger:
+app.use(require('./src/middlewares/logger'))
+
+//? Searching&Sorting&Paginations: res.getModelList():
+app.use(require('./src/middlewares/findSearchSortPage'))
+
+
+/* ------------------------------------------------ */
+//! Routes:
+
+//? Home Path:
+app.all('/', (req, res) => {
+    res.send({
+        error: false,
+        message: 'Welcome to Stock API',
+        documents: {
+            swagger: '/documents/swagger',
+            redoc: '/document/redoc',
+            json: '/documents/json'
+        }
+    })
+})
+
+
+//? Routes:
+app.use(require('./src/routes/index'))
+
+/* ------------------------------------------------ */
+//? Error Handler:
+app.use(require('./src/middlewares/errorHandler'))
+
+//? Run Server:
+app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`))
