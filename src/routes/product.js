dfed7eecf2a097a39ef612164f96a3bd0c455468
DGKN <sakardogukan@gmail.com>
2023-12-17 22:51:52 +0300

    product.js route file added

---
+++ src/routes/product.js
@@ -0,0 +1,19 @@
+"use strict"
+
+/* ----------------- PRODUCT ROUTE ------------------- */
+
+const router = require('express').Router()
+
+const product = require('../controllers/product')
+
+router.route('/')
+    .get(product.list)
+    .post(product.create)
+
+router.route('/:id')
+    .get(product.read)
+    .put(product.update)
+    .patch(product.update)
+    .delete(product.delete)
+
+module.exports = router
