b8ee9356c5b8f89954be077b3327419d02ca74cb
DGKN <sakardogukan@gmail.com>
2023-12-17 22:52:32 +0300

    token.js route file added

---
+++ src/routes/token.js
@@ -0,0 +1,19 @@
+"use strict"
+
+/* -------------------- TOKEN ROUTE --------------- */
+
+const router = require('express').Router()
+
+const token = require('../controllers/token')
+
+router.route('/')
+    .get(token.list)
+    .post(token.create)
+
+router.route('/:id')
+    .get(token.read)
+    .put(token.update)
+    .patch(token.update)
+    .delete(token.delete)
+
+module.exports = router
