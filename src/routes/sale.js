2cf33bdc8f50334e18ead00eaab236402da042ad
DGKN <sakardogukan@gmail.com>
2023-12-17 22:52:20 +0300

    sale.js route file added

---
+++ src/routes/sale.js
@@ -0,0 +1,19 @@
+"use strict"
+
+/* ----------------- SALE ROUTE ------------------- */
+
+const router = require('express').Router()
+
+const sale = require('../controllers/sale')
+
+router.route('/')
+    .get(sale.list)
+    .post(sale.create)
+
+router.route('/:id')
+    .get(sale.read)
+    .put(sale.update)
+    .patch(sale.update)
+    .delete(sale.delete)
+
+module.exports = router
