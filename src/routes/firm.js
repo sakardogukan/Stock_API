a2ab76f265d78e109bd09ecbcbf90b5825869908
DGKN <sakardogukan@gmail.com>
2023-12-17 22:51:18 +0300

    firm.js route file added

---
+++ src/routes/firm.js
@@ -0,0 +1,19 @@
+"use strict"
+
+/* ----------------- FIRM ROUTE ------------------- */
+
+const router = require('express').Router()
+
+const firm = require('../controllers/firm')
+
+router.route('/')
+    .get(firm.list)
+    .post(firm.create)
+
+router.route('/:id')
+    .get(firm.read)
+    .put(firm.update)
+    .patch(firm.update)
+    .delete(firm.delete)
+
+module.exports = router
