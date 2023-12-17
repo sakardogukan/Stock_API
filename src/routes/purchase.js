9fb7e19951b8a009b14c16897a1b94b6d57a420e
DGKN <sakardogukan@gmail.com>
2023-12-17 22:52:06 +0300

    purchase.js route file added

---
+++ src/routes/purchase.js
@@ -0,0 +1,19 @@
+"use strict"
+
+/* ----------------- PURCHASE ROUTE ------------------- */
+
+const router = require('express').Router()
+
+const purchase = require('../controllers/purchase')
+
+router.route('/')
+    .get(purchase.list)
+    .post(purchase.create)
+
+router.route('/:id')
+    .get(purchase.read)
+    .put(purchase.update)
+    .patch(purchase.update)
+    .delete(purchase.delete)
+
+module.exports = router
