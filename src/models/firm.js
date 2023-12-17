9290c03aea754a24776e6469a5b7464cb8b5c7b7
DGKN <sakardogukan@gmail.com>
2023-12-17 22:49:20 +0300

    firm.js model file added

---
+++ src/models/firm.js
@@ -0,0 +1,33 @@
+"use strict"
+
+/* ----------------- FIRM MODEL ------------------- */
+
+const { mongoose } = require('../config/dbConnection')
+
+const FirmSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        trim: true
+    },
+    address: {
+        type: String,
+        trim: true
+    },
+    image: { // URL
+        type: String,
+        trim: true
+    }
+}, { collection: 'firms', timestamps: true, versionKey: false })
+
+FirmSchema.pre('init', function (data) {
+    data.id = data._id
+    data.createds = data.createdAt.toLocaleDateString('tr-tr')
+})
+
+module.exports = mongoose.model('Firm', FirmSchema)
