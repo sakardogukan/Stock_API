7de08f241fa89f4ebb8f3e84b5ebd2f6f8bd2bcd
DGKN <sakardogukan@gmail.com>
2023-12-17 22:51:38 +0300

    index.js route file updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,26 @@
 
 const router = require('express').Router()
 
-
-
-// User:'
+// User:
 router.use('/users', require('./user.js'))
+// Token:
+router.use('/tokens', require('./token'))
+// Brand:
+router.use('brands', require('./brand.js'))
+// Category:
+router.use('categories', require('./category.js'))
+// Firm:
+router.use('firms', require('./firm.js'))
+// Product:
+router.use('products', require('./product.js'))
+// Purchase:
+router.use('purchases', require('./purchase.js'))
+// Sale:
+router.use('sales', require('./sale.js'))
 
 // Document:
 router.use('/documents', require('./document.js'))
+
+
 
 module.exports = router
