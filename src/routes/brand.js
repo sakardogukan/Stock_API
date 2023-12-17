0dd51dfa770be956bf10c201088b49ca8edbab80
DGKN <sakardogukan@gmail.com>
2023-12-17 22:50:47 +0300

    brand.js route file added

---
+++ src/routes/brand.js
@@ -0,0 +1,19 @@
+"use strict"
+
+/* ----------------- BRAND ROUTE ------------------- */
+
+const router = require('express').Router()
+
+const brand = require('../controllers/brand')
+
+router.route('/')
+    .get(brand.list)
+    .post(brand.create)
+
+router.route('/:id')
+    .get(brand.read)
+    .put(brand.update)
+    .patch(brand.update)
+    .delete(brand.delete)
+
+module.exports = router
