2f33466bb441bb1c8435d2f7683bbdc1cdf7f507
DGKN <sakardogukan@gmail.com>
2023-12-19 23:33:00 +0300

    category.js route permissions added

--- src/routes/category.js
+++ src/routes/category.js
@@ -4,16 +4,18 @@
 
 const router = require('express').Router()
 
+const permissions = require('../middlewares/permissions')
+
 const category = require('../controllers/category')
 
 router.route('/')
-    .get(category.list)
-    .post(category.create)
+    .get(permissions.isStaff, category.list)
+    .post(permissions.isAdmin, category.create)
 
 router.route('/:id')
-    .get(category.read)
-    .put(category.update)
-    .patch(category.update)
-    .delete(category.delete)
+    .get(permissions.isStaff, category.read)
+    .put(permissions.isAdmin, category.update)
+    .patch(permissions.isAdmin, category.update)
+    .delete(permissions.isAdmin, category.delete)
 
 module.exports = router
