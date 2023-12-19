7380a03bf14f854cbfbdbcbbaa1389e5ce3a0519
DGKN <sakardogukan@gmail.com>
2023-12-19 23:34:53 +0300

    permissions middlewares file updated

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -2,3 +2,32 @@
 
 /* ----------------- PERMISSIONS ------------------ */
 
+module.exports = {
+    isLogin: (req, res, next) => {
+
+        if (req.user && req.user.is_active) {
+            next()
+        } else {
+            res.errorStatusCode = 403
+            throw new Error('No permission: You must login.')
+        }
+    },
+    isAdmin: (req, res, next) => {
+
+        if (req.user && req.user.is_active && req.user.superadmin) {
+            next()
+        } else {
+            res.errorStatusCode = 403
+            throw new Error('No permission: You must login and to be admin.')
+        }
+    },
+    isStaff: (req, res, next) => {
+
+        if (req.user && req.user.is_active && (req.user.is_superadmin || req.user.is_staff)) {
+            next()
+        } else {
+            res.errorStatusCode = 403
+            throw new Error('No permission: You must login and to be staff.')
+        }
+    }
+}
